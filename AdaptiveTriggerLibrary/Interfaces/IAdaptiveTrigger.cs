f85372d81ff08cf96fdf9e23f8c7b39a2b962400
Stefan Over <herdosratos@googlemail.com>
2015-07-30 21:31:01 +0200

    Fixed / Implemented NuGet package generation

--- AdaptiveTriggerLibrary/Interfaces/IAdaptiveTrigger.cs
+++ AdaptiveTriggerLibrary/Interfaces/IAdaptiveTrigger.cs
@@ -2,11 +2,20 @@
 {
     using System;
 
+    /// <summary>
+    /// The base interface for all adaptive triggers in the project.
+    /// </summary>
+    /// <typeparam name="TCondition">The type of the <see cref="Condition"/>.</typeparam>
+    /// <typeparam name="TConditionModifier">The type of the <see cref="ConditionModifier"/>, that can influence the way that the <see cref="Condition"/> is treated.</typeparam>
     public interface IAdaptiveTrigger<TCondition, TConditionModifier>
         where TConditionModifier : IConditionModifier
     {
         event EventHandler IsActiveChanged;
 
         bool IsActive { get; }
+
+        TCondition Condition { get; set; }
+
+        TConditionModifier ConditionModifier { get; set; }
     }
 }
---
+++ nuget/build-nupkg.cmd
@@ -0,0 +1,43 @@
+@ECHO OFF
+
+REM
+REM Version is read from the VERSION file.
+REM
+
+SETLOCAL
+
+PUSHD "%~dp0"
+
+WHERE /Q nuget >NUL
+IF %ERRORLEVEL% NEQ 0 ( 
+    ECHO nuget not found.
+    ECHO.
+    ECHO Run "%~pd0download-nuget.cmd" to download the latest version, or update PATH as appropriate.
+    GOTO END
+)
+
+SET /p VERSION=<VERSION
+
+SET BIN=\AdaptiveTriggerLibrary\AdaptiveTriggerLibrary\bin
+SET OUTDIR=..\nugetRelease
+SET LICENSE_URL=https://github.com/Herdo/AdaptiveTriggerLibrary/blob/master/LICENSE
+SET REQUIRE_LICENSE_ACCEPTANCE=true
+
+IF NOT "%1" == "" (
+    SET VERSION=%VERSION%-%1
+)
+
+SET NUGET_ARGS=^
+    -nopackageanalysis ^
+    -basepath ..\.. ^
+    -outputdirectory %OUTDIR% ^
+    -version %VERSION% ^
+    -properties bin=%BIN%;LicenseUrl=%LICENSE_URL%;RequireLicenseAcceptance=%REQUIRE_LICENSE_ACCEPTANCE%
+
+nuget pack AdaptiveTriggerLibrary.uwp.nuspec %NUGET_ARGS%
+IF %ERRORLEVEL% NEQ 0 GOTO END
+
+:END
+
+POPD
+EXIT /B %ERRORLEVEL%
