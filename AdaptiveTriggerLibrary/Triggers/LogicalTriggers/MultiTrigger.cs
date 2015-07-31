3eccffb0b360613498f654a6219a3cb5bb4e6ce9
Stefan Over <herdosratos@googlemail.com>
2015-07-31 20:25:08 +0200

    Actual multi trigger implementation

---
+++ AdaptiveTriggerLibrary/Triggers/LogicalTriggers/MultiTrigger.cs
@@ -0,0 +1,85 @@
+﻿namespace AdaptiveTriggerLibrary.Triggers.LogicalTriggers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using ConditionModifiers.LogicalModifiers;
+
+    /// <summary>
+    /// This trigger activates, if any or all (depending on <see cref="AdaptiveTriggerBase{TCondition,TConditionModifier}.ConditionModifier"/>)
+    /// of the specified <see cref="Triggers"/> matches the <see cref="AdaptiveTriggerBase{TCondition,TConditionModifier}.Condition"/>.
+    /// </summary>
+    public class MultiTrigger : AdaptiveTriggerBase<bool, ILogicalModifier>
+    {
+        ///////////////////////////////////////////////////////////////////
+        #region Fields
+
+        private IList<IAdaptiveTrigger> _triggers;
+
+        #endregion
+
+        ///////////////////////////////////////////////////////////////////
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the triggers.
+        /// </summary>
+        public IList<IAdaptiveTrigger> Triggers
+        {
+            get { return _triggers; }
+            set
+            {
+                if (_triggers != null)
+                    foreach (var trigger in _triggers)
+                        trigger.IsActiveChanged -= Trigger_IsActiveChanged;
+                _triggers = value;
+                if (_triggers != null)
+                    foreach (var trigger in _triggers)
+                        trigger.IsActiveChanged += Trigger_IsActiveChanged;
+                CurrentValue = GetCurrentValue();
+            }
+        }
+
+        #endregion
+
+        ///////////////////////////////////////////////////////////////////
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MultiTrigger"/> class.
+        /// Default condition: true.
+        /// Default modifier: <see cref="AndModifier"/>.
+        /// </summary>
+        public MultiTrigger()
+            : base(true, new AndModifier())
+        {
+        }
+
+        #endregion
+
+        ///////////////////////////////////////////////////////////////////
+        #region Private Methods
+
+        private bool GetCurrentValue()
+        {
+            if (Triggers == null
+             || Triggers.Count == 0)
+            {
+                return false;
+            }
+            return ConditionModifier.IsConditionMet(Condition, Triggers.Select(m => m.IsActive).ToArray());
+        }
+
+        #endregion
+
+        ///////////////////////////////////////////////////////////////////
+        #region Event Handler
+
+        private void Trigger_IsActiveChanged(object sender, EventArgs e)
+        {
+            CurrentValue = GetCurrentValue();
+        }
+
+        #endregion
+    }
+}
