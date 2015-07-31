0cb5a672505e0222eb3aed547e5802fbdfbd85a6
Stefan Over <herdosratos@googlemail.com>
2015-07-31 22:25:48 +0200

    Added unit tests for equals modifier

---
+++ AdaptiveTriggerLibrary.UnitTestApp/Tests/ConditionModifierTests/EqualsModifierTest.cs
@@ -0,0 +1,38 @@
+﻿namespace AdaptiveTriggerLibrary.UnitTestApp.Tests.ConditionModifierTests
+{
+    using Windows.UI.ViewManagement;
+    using ConditionModifiers.GenericModifiers;
+    using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+    [TestClass]
+    public class EqualsModifierTest
+    {
+        [TestMethod]
+        public void Equals_True()
+        {
+            // Arrange
+            bool result;
+            var modifier = new EqualsModifier<UserInteractionMode>();
+
+            // Act
+            result = modifier.IsConditionMet(UserInteractionMode.Mouse, UserInteractionMode.Mouse);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Equals_False()
+        {
+            // Arrange
+            bool result;
+            var modifier = new EqualsModifier<UserInteractionMode>();
+
+            // Act
+            result = modifier.IsConditionMet(UserInteractionMode.Mouse, UserInteractionMode.Touch);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
