224c12f708d1009670a6eaa0759845e8f965d5eb
Stefan Over <herdosratos@googlemail.com>
2015-07-31 18:05:33 +0200

    Added unit test app and first tests

---
+++ AdaptiveTriggerLibrary.UnitTestApp/Tests/ConditionModifierTests/EqualToModifierTest.cs
@@ -0,0 +1,37 @@
+﻿namespace AdaptiveTriggerLibrary.UnitTestApp.Tests.ConditionModifierTests
+{
+    using ConditionModifiers.ComparableModifiers;
+    using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+    [TestClass]
+    public class EqualToModifierTest
+    {
+        [TestMethod]
+        public void Equals_Bool_True()
+        {
+            // Arrange
+            bool result;
+            var modifier = new EqualToModifier();
+
+            // Act
+            result = modifier.IsConditionMet(true, true);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Equals_Bool_False()
+        {
+            // Arrange
+            bool result;
+            var modifier = new EqualToModifier();
+
+            // Act
+            result = modifier.IsConditionMet(true, false);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ AdaptiveTriggerLibrary.UnitTestApp/Tests/TriggerTests/DeviceFamilyTriggerTest.cs
@@ -0,0 +1,22 @@
+﻿namespace AdaptiveTriggerLibrary.UnitTestApp.Tests.TriggerTests
+{
+    using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+    using Triggers.HardwareInterfaceTriggers;
+
+    [TestClass]
+    public class DeviceFamilyTriggerTest
+    {
+        [TestMethod]
+        public void IsActive_Ok()
+        {
+            // Arrange
+            var trigger = new DeviceFamilyTrigger();
+
+            // Act
+            trigger.Condition = "PC";
+
+            // Assert
+            Assert.IsTrue(trigger.IsActive);
+        }
+    }
+}
