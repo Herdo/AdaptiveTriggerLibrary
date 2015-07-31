879b9268dbb7243c550dc4427602ed907585ea80
Stefan Over <herdosratos@googlemail.com>
2015-07-31 21:48:50 +0200

    Added logical modifier unit tests

---
+++ AdaptiveTriggerLibrary.UnitTestApp/Tests/ConditionModifierTests/OrModifierTest.cs
@@ -0,0 +1,81 @@
+﻿namespace AdaptiveTriggerLibrary.UnitTestApp.Tests.ConditionModifierTests
+{
+    using ConditionModifiers.LogicalModifiers;
+    using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+    [TestClass]
+    public class OrModifierTest
+    {
+        [TestMethod]
+        public void AllTrue_Single()
+        {
+            // Arrange
+            bool result;
+            var modifier = new OrModifier();
+
+            // Act
+            result = modifier.IsConditionMet(true, true);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void AllTrue_Multiple()
+        {
+            // Arrange
+            bool result;
+            var inputs = new[] { true, true, true };
+            var modifier = new OrModifier();
+
+            // Act
+            result = modifier.IsConditionMet(true, inputs);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void AllTrue_Null()
+        {
+            // Arrange
+            bool result;
+            var modifier = new OrModifier();
+
+            // Act
+            result = modifier.IsConditionMet(true, null);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void NotAllTrue_Single()
+        {
+            // Arrange
+            bool result;
+            var modifier = new OrModifier();
+
+            // Act
+            result = modifier.IsConditionMet(true, false);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void NotAllTrue_Multiple()
+        {
+            // Arrange
+            bool result;
+            var inputs = new[] { true, false, true };
+            var modifier = new OrModifier();
+
+            // Act
+            result = modifier.IsConditionMet(true, inputs);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+    }
+}
