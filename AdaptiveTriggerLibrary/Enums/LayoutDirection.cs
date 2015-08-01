fc90388172d26a5fe1dadd8b34ae134a39a6a9d7
Stefan Over <herdosratos@googlemail.com>
2015-08-01 22:52:57 +0200

    Added LayoutDirection trigger

---
+++ AdaptiveTriggerLibrary/Enums/LayoutDirection.cs
@@ -0,0 +1,26 @@
+﻿// ReSharper disable InconsistentNaming
+namespace AdaptiveTriggerLibrary.Enums
+{
+    /// <summary>
+    /// Defines constants that specify a layout direction for the UI.
+    /// </summary>
+    public enum LayoutDirection
+    {
+        /// <summary>
+        /// Layout direction is left to right.
+        /// </summary>
+        LTR = 0,
+        /// <summary>
+        /// Layout direction is right to left.
+        /// </summary>
+        RTL = 1,
+        /// <summary>
+        /// Layout direction is top to bottom and left to right.
+        /// </summary>
+        TTBLTR = 2,
+        /// <summary>
+        /// Layout direction is top to bottom and right to left.
+        /// </summary>
+        TTBRTL = 3
+    }
+}
---
+++ AdaptiveTriggerLibrary/Triggers/UserInterfaceTriggers/LayoutDirectionTrigger.cs
@@ -0,0 +1,48 @@
+﻿namespace AdaptiveTriggerLibrary.Triggers.UserInterfaceTriggers
+{
+    using System;
+    using Windows.ApplicationModel.Resources.Core;
+    using ConditionModifiers.ComparableModifiers;
+    using Enums;
+
+    /// <summary>
+    /// This trigger activates, if the current scale
+    /// matches the specified <see cref="AdaptiveTriggerBase{TCondition,TConditionModifier}.Condition"/>.
+    /// </summary>
+    public class LayoutDirectionTrigger : AdaptiveTriggerBase<LayoutDirection, IComparableModifier>
+    {
+        ///////////////////////////////////////////////////////////////////
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LayoutDirectionTrigger"/> class.
+        /// Default modifier: <see cref="EqualToModifier"/>.
+        /// </summary>
+        public LayoutDirectionTrigger()
+            : base(new EqualToModifier())
+        {
+            // Set initial value
+            CurrentValue = GetCurrentValue();
+        }
+
+        #endregion
+
+        ///////////////////////////////////////////////////////////////////
+        #region Private Methods
+
+        private LayoutDirection GetCurrentValue()
+        {
+            var result = LayoutDirection.LTR;
+            var qualifiers = ResourceContext.GetForCurrentView().QualifierValues;
+            if (qualifiers.ContainsKey("LayoutDirection"))
+            {
+                LayoutDirection parsed;
+                if (Enum.TryParse(qualifiers["LayoutDirection"], out parsed))
+                    result = parsed;
+            }
+            return result;
+        }
+
+        #endregion
+    }
+}
