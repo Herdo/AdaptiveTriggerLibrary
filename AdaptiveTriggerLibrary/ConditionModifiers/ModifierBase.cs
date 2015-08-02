db5615689ee6ad30684aa36f808472b79de16c78
Stefan Over <herdosratos@googlemail.com>
2015-08-02 11:13:42 +0200

    Added XML docu

--- AdaptiveTriggerLibrary/ConditionModifiers/ModifierBase.cs
+++ AdaptiveTriggerLibrary/ConditionModifiers/ModifierBase.cs
@@ -10,6 +10,9 @@
         ///////////////////////////////////////////////////////////////////
         #region Properties
 
+        /// <summary>
+        /// Message that will be displayed when all calls to <see cref="TryGetParsedParameters{TConditionType,TValueType}"/> fail.
+        /// </summary>
         protected const string InvalidCastMessage =
             "Parameters could not be parsed to a type that the method can handle.";
 
@@ -18,6 +21,16 @@
         ///////////////////////////////////////////////////////////////////
         #region Protected Methods
 
+        /// <summary>
+        /// Trys to parse the <paramref name="condition"/> and <paramref name="value"/>
+        /// into the underlying types <typeparamref name="TConditionType"/> and <typeparamref name="TValueType"/>.
+        ///  </summary>
+        /// <typeparam name="TConditionType">The underlying type for <paramref name="condition"/>.</typeparam>
+        /// <typeparam name="TValueType">The underlying type for <paramref name="value"/>.</typeparam>
+        /// <param name="condition">The condition to parse.</param>
+        /// <param name="value">The value to parse.</param>
+        /// <param name="parsedParameters">The parsed objects on success, otherwise null.</param>
+        /// <returns>True, if the parsing was successfull, otherwise false.</returns>
         protected bool TryGetParsedParameters<TConditionType, TValueType>(object condition, object value, out Tuple<TConditionType, TValueType> parsedParameters)
         {
             var conditionConvertible = condition is TConditionType;
