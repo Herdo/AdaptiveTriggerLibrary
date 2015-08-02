b9cf87520d096f99a0d60b0b99810eb16b99f766
Stefan Over <herdosratos@googlemail.com>
2015-08-02 18:07:48 +0200

    Added IsRoaming data trigger

---
+++ AdaptiveTriggerLibrary/Triggers/ConnectivityTriggers/IsRoamingDataTrigger.cs
@@ -0,0 +1,52 @@
+﻿namespace AdaptiveTriggerLibrary.Triggers.ConnectivityTriggers
+{
+    using Windows.Networking.Connectivity;
+    using ConditionModifiers.GenericModifiers;
+
+    /// <summary>
+    /// This trigger activates, if the connection being established is outside of the home provider
+    /// matches the specified <see cref="AdaptiveTriggerBase{TCondition,TConditionModifier}.Condition"/>.
+    /// </summary>
+    public class IsRoamingDataTrigger : AdaptiveTriggerBase<bool, IGenericModifier>
+    {
+        ///////////////////////////////////////////////////////////////////
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IsRoamingDataTrigger"/> class.
+        /// Default modifier: <see cref="EqualsModifier{Boolean}"/>.
+        /// </summary>
+        public IsRoamingDataTrigger()
+            : base(new EqualsModifier<bool>())
+        {
+            // Subscribe to state changed event
+            NetworkInformation.NetworkStatusChanged += NetworkInformation_NetworkStatusChanged;
+
+            // Set initial value
+            CurrentValue = GetCurrentValue();
+        }
+
+        #endregion
+
+        ///////////////////////////////////////////////////////////////////
+        #region Private Methods
+
+        private static bool GetCurrentValue()
+        {
+            var profile = NetworkInformation.GetInternetConnectionProfile();
+            return profile?.GetConnectionCost().Roaming ?? false;
+        }
+
+        #endregion
+
+        ///////////////////////////////////////////////////////////////////
+        #region Event Handler
+
+        private void NetworkInformation_NetworkStatusChanged(object sender)
+        {
+            CurrentValue = GetCurrentValue();
+        }
+
+        #endregion
+    }
+}
