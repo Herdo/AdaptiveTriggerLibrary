b442a829a5d45593c772b62d4b077d736b646d04
Stefan Over <herdosratos@googlemail.com>
2015-08-02 17:59:53 +0200

    Added signal stength trigger

---
+++ AdaptiveTriggerLibrary/Triggers/ConnectivityTriggers/SignalStrengthTrigger.cs
@@ -0,0 +1,52 @@
+﻿namespace AdaptiveTriggerLibrary.Triggers.ConnectivityTriggers
+{
+    using Windows.Networking.Connectivity;
+    using ConditionModifiers.ComparableModifiers;
+
+    /// <summary>
+    /// This trigger activates, if the signal strength
+    /// matches the specified <see cref="AdaptiveTriggerBase{TCondition,TConditionModifier}.Condition"/>.
+    /// </summary>
+    public class SignalStrengthTrigger : AdaptiveTriggerBase<byte, IComparableModifier>
+    {
+        ///////////////////////////////////////////////////////////////////
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SignalStrengthTrigger"/> class.
+        /// Default modifier: <see cref="EqualToModifier"/>.
+        /// </summary>
+        public SignalStrengthTrigger()
+            : base(new EqualToModifier())
+        {
+            // Subscribe to state changed event
+            NetworkInformation.NetworkStatusChanged += NetworkInformation_NetworkStatusChanged;
+
+            // Set initial value
+            CurrentValue = GetCurrentValue();
+        }
+
+        #endregion
+
+        ///////////////////////////////////////////////////////////////////
+        #region Private Methods
+
+        private static byte GetCurrentValue()
+        {
+            var profile = NetworkInformation.GetInternetConnectionProfile();
+            return profile?.GetSignalBars() ?? 0;
+        }
+
+        #endregion
+
+        ///////////////////////////////////////////////////////////////////
+        #region Event Handler
+            
+        private void NetworkInformation_NetworkStatusChanged(object sender)
+        {
+            CurrentValue = GetCurrentValue();
+        }
+
+        #endregion
+    }
+}
