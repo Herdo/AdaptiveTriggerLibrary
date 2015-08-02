32e37a228c84624021c56dfb624b287d478552ce
Stefan Over <herdosratos@googlemail.com>
2015-08-02 18:43:40 +0200

    Added XOR logical modifier

---
+++ AdaptiveTriggerLibrary.UnitTestApp/Tests/ConditionModifierTests/XORModifierTest.cs
@@ -0,0 +1,111 @@
+﻿namespace AdaptiveTriggerLibrary.UnitTestApp.Tests.ConditionModifierTests
+{
+    using System;
+    using System.Linq;
+    using ConditionModifiers;
+    using ConditionModifiers.LogicalModifiers;
+    using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+    [TestClass]
+    public class XORModifierTest
+    {
+        [TestMethod]
+        public void XOR_InvalidCast()
+        {
+            // Arrange
+            IConditionModifier modifier = new XORModifier();
+
+            // Act
+            Action action = () => modifier.IsConditionMet(null, null);
+
+            // Assert
+            Assert.ThrowsException<InvalidCastException>(action);
+        }
+
+        [TestMethod]
+        public void XOR_AllTrue_Single()
+        {
+            // Arrange
+            bool result;
+            IConditionModifier modifier = new XORModifier();
+
+            // Act
+            result = modifier.IsConditionMet(true, true);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void XOR_AllTrue_Multiple()
+        {
+            // Arrange
+            bool result;
+            var inputs = new[] { true, true, true };
+            IConditionModifier modifier = new XORModifier();
+
+            // Act
+            result = modifier.IsConditionMet(true, inputs.ToArray());
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void XOR_AllTrue_Null()
+        {
+            // Arrange
+            IConditionModifier modifier = new XORModifier();
+
+            // Act
+            Action action = () => modifier.IsConditionMet(true, null);
+
+            // Assert
+            Assert.ThrowsException<InvalidCastException>(action);
+        }
+
+        [TestMethod]
+        public void XOR_NotAllTrue_Single()
+        {
+            // Arrange
+            bool result;
+            IConditionModifier modifier = new XORModifier();
+
+            // Act
+            result = modifier.IsConditionMet(true, false);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void XOR_NotAllTrue_Multiple()
+        {
+            // Arrange
+            bool result;
+            var inputs = new[] { true, false, true };
+            IConditionModifier modifier = new XORModifier();
+
+            // Act
+            result = modifier.IsConditionMet(false, inputs);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void XOR_NotAllTrue_MultipleInverted()
+        {
+            // Arrange
+            bool result;
+            var inputs = new[] { true, false, true };
+            IConditionModifier modifier = new XORModifier();
+
+            // Act
+            result = modifier.IsConditionMet(true, inputs);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ AdaptiveTriggerLibrary/ConditionModifiers/LogicalModifiers/XORModifier.cs
@@ -0,0 +1,58 @@
+﻿namespace AdaptiveTriggerLibrary.ConditionModifiers.LogicalModifiers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// A modifier where only a single value must meet the condition.
+    /// If more than one value meets the condition, the condition is not met.
+    /// </summary>
+    public class XORModifier : ModifierBase,
+                               ILogicalModifier
+    {
+        ///////////////////////////////////////////////////////////////////
+        #region Private Methods
+
+        private static bool IsConditionMet(bool condition, IEnumerable<bool> values)
+        {
+            return values?.Count(m => m == condition) == 1;
+        }
+
+        #endregion
+
+        ///////////////////////////////////////////////////////////////////
+        #region ILogicalModifier
+
+        /// <summary>
+        /// Checks if the <paramref name="value"/> meets the specified <paramref name="condition"/>.
+        /// </summary>
+        /// <param name="condition">The condition.</param>
+        /// <param name="value">The actual value.</param>
+        /// <exception cref="ArgumentException">The underlying type of <paramref name="condition"/> doesn't match expected condition type,
+        /// or the underlying type of<paramref name="value"/> doesn't match the expected value type.</exception>
+        /// <exception cref="InvalidCastException">Either <paramref name="condition"/> or <paramref name="value"/> cannot be casted to the specified underlying type.</exception>
+        /// <returns>True, if the <paramref name="value"/> meets the specified <paramref name="condition"/>, otherwise false.</returns>
+        bool IConditionModifier.IsConditionMet(object condition, object value)
+        {
+            Tuple<bool, bool> singleValueParameters;
+            Tuple<bool, IEnumerable<bool>> multiValueParameters;
+            
+            var parsed = TryGetParsedParameters(condition, value, out singleValueParameters);
+
+            // Support single values
+            if (parsed)
+                return IsConditionMet(singleValueParameters.Item1, new[] {singleValueParameters.Item2});
+
+            // Support multiple values
+            parsed = TryGetParsedParameters(condition, value, out multiValueParameters);
+            if (parsed)
+                return IsConditionMet(multiValueParameters.Item1, multiValueParameters.Item2);
+
+            // Cast failed
+            throw new InvalidCastException(InvalidCastMessage);
+        }
+
+        #endregion
+    }
+}
