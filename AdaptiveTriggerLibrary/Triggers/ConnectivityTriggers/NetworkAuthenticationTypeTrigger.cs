b527e8cdf4e8043a62d94a8b2767b5615d573811
Stefan Over <herdosratos@googlemail.com>
2015-08-02 18:04:38 +0200

    Added network auth and encryption type triggers

---
+++ AdaptiveTriggerLibrary/Triggers/ConnectivityTriggers/NetworkAuthenticationTypeTrigger.cs
@@ -0,0 +1,52 @@
+﻿namespace AdaptiveTriggerLibrary.Triggers.ConnectivityTriggers
+{
+    using Windows.Networking.Connectivity;
+    using ConditionModifiers.ComparableModifiers;
+
+    /// <summary>
+    /// This trigger activates, if the network authentication type
+    /// matches the specified <see cref="AdaptiveTriggerBase{TCondition,TConditionModifier}.Condition"/>.
+    /// </summary>
+    public class NetworkAuthenticationTypeTrigger : AdaptiveTriggerBase<NetworkAuthenticationType, IComparableModifier>
+    {
+        ///////////////////////////////////////////////////////////////////
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NetworkAuthenticationTypeTrigger"/> class.
+        /// Default modifier: <see cref="EqualToModifier"/>.
+        /// </summary>
+        public NetworkAuthenticationTypeTrigger()
+            : base(new EqualToModifier())
+        {
+            // Subscribe to state changed event
+            NetworkInformation.NetworkStatusChanged += NetworkInformation_NetworkStatusChanged;
+
+            // Set initial value
+            CurrentValue = GetCurrentValue();
+        }
+
+        #endregion
+
+        ///////////////////////////////////////////////////////////////////
+        #region Private Methods
+
+        private static NetworkAuthenticationType GetCurrentValue()
+        {
+            var profile = NetworkInformation.GetInternetConnectionProfile();
+            return profile?.NetworkSecuritySettings.NetworkAuthenticationType ?? default(NetworkAuthenticationType);
+        }
+
+        #endregion
+
+        ///////////////////////////////////////////////////////////////////
+        #region Event Handler
+            
+        private void NetworkInformation_NetworkStatusChanged(object sender)
+        {
+            CurrentValue = GetCurrentValue();
+        }
+
+        #endregion
+    }
+}
---
+++ AdaptiveTriggerLibrary/Triggers/ConnectivityTriggers/NetworkEncryptionTypeTrigger.cs
@@ -0,0 +1,52 @@
+﻿namespace AdaptiveTriggerLibrary.Triggers.ConnectivityTriggers
+{
+    using Windows.Networking.Connectivity;
+    using ConditionModifiers.ComparableModifiers;
+
+    /// <summary>
+    /// This trigger activates, if the network authentication type
+    /// matches the specified <see cref="AdaptiveTriggerBase{TCondition,TConditionModifier}.Condition"/>.
+    /// </summary>
+    public class NetworkEncryptionTypeTrigger : AdaptiveTriggerBase<NetworkEncryptionType, IComparableModifier>
+    {
+        ///////////////////////////////////////////////////////////////////
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NetworkEncryptionTypeTrigger"/> class.
+        /// Default modifier: <see cref="EqualToModifier"/>.
+        /// </summary>
+        public NetworkEncryptionTypeTrigger()
+            : base(new EqualToModifier())
+        {
+            // Subscribe to state changed event
+            NetworkInformation.NetworkStatusChanged += NetworkInformation_NetworkStatusChanged;
+
+            // Set initial value
+            CurrentValue = GetCurrentValue();
+        }
+
+        #endregion
+
+        ///////////////////////////////////////////////////////////////////
+        #region Private Methods
+
+        private static NetworkEncryptionType GetCurrentValue()
+        {
+            var profile = NetworkInformation.GetInternetConnectionProfile();
+            return profile?.NetworkSecuritySettings.NetworkEncryptionType ?? default(NetworkEncryptionType);
+        }
+
+        #endregion
+
+        ///////////////////////////////////////////////////////////////////
+        #region Event Handler
+            
+        private void NetworkInformation_NetworkStatusChanged(object sender)
+        {
+            CurrentValue = GetCurrentValue();
+        }
+
+        #endregion
+    }
+}
