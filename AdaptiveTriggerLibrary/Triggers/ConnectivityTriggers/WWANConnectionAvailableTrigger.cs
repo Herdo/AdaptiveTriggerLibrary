a0f930c9d5348f56a4d68e4b7c07ecd663cfff52
Stefan Over <herdosratos@googlemail.com>
2015-08-02 17:56:16 +0200

    Added WWAN and WLAN available trigger

---
+++ AdaptiveTriggerLibrary/Triggers/ConnectivityTriggers/WWANConnectionAvailableTrigger.cs
@@ -0,0 +1,53 @@
+﻿namespace AdaptiveTriggerLibrary.Triggers.ConnectivityTriggers
+{
+    using Windows.Networking.Connectivity;
+    using ConditionModifiers.GenericModifiers;
+
+    /// <summary>
+    /// This trigger activates, if the current internet connection profile being a WWAN (mobile) connection profile
+    /// matches the specified <see cref="AdaptiveTriggerBase{TCondition,TConditionModifier}.Condition"/>.
+    /// </summary>
+    public class WWANConnectionAvailableTrigger : AdaptiveTriggerBase<bool, IGenericModifier>
+    {
+        ///////////////////////////////////////////////////////////////////
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WWANConnectionAvailableTrigger"/> class.
+        /// Default condition: true.
+        /// Default modifier: <see cref="EqualsModifier{Boolean}"/>.
+        /// </summary>
+        public WWANConnectionAvailableTrigger()
+            : base(true, new EqualsModifier<bool>())
+        {
+            // Subscribe to state changed event
+            NetworkInformation.NetworkStatusChanged += NetworkInformation_NetworkStatusChanged;
+
+            // Set initial value
+            CurrentValue = GetCurrentValue();
+        }
+
+        #endregion
+
+        ///////////////////////////////////////////////////////////////////
+        #region Private Methods
+
+        private static bool GetCurrentValue()
+        {
+            var profile = NetworkInformation.GetInternetConnectionProfile();
+            return profile?.IsWwanConnectionProfile ?? false;
+        }
+
+        #endregion
+
+        ///////////////////////////////////////////////////////////////////
+        #region Event Handler
+            
+        private void NetworkInformation_NetworkStatusChanged(object sender)
+        {
+            CurrentValue = GetCurrentValue();
+        }
+
+        #endregion
+    }
+}
