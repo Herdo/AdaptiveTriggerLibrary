553128ca6e11a97571b8de8ed5e08746a5b69895
Stefan Over <herdosratos@googlemail.com>
2015-08-11 18:19:38 +0200

    Added Window Size Triggers

---
+++ AdaptiveTriggerLibrary/Triggers/UserInterfaceTriggers/WindowHeightTrigger.cs
@@ -0,0 +1,54 @@
+﻿namespace AdaptiveTriggerLibrary.Triggers.UserInterfaceTriggers
+{
+    using Windows.UI.Core;
+    using Windows.UI.Xaml;
+    using ConditionModifiers.ComparableModifiers;
+    using Functional;
+
+    /// <summary>
+    /// This trigger activates, if the current window height
+    /// matches the specified <see cref="AdaptiveTriggerBase{TCondition,TConditionModifier}.Condition"/>.
+    /// </summary>
+    /// <remarks>Use this trigger as alternative to the <see cref="AdaptiveTrigger"/>.</remarks>
+    public class WindowHeightTrigger : AdaptiveTriggerBase<double, IComparableModifier>
+    {
+        ///////////////////////////////////////////////////////////////////
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WindowHeightTrigger"/> class.
+        /// Default modifier: <see cref="GreaterThanEqualToModifier"/>.
+        /// </summary>
+        public WindowHeightTrigger()
+            : base(new GreaterThanEqualToModifier())
+        {
+            // Create a weak subscription to the SizeChanged event so that we don't pin the trigger or page in memory
+            WeakEvent.Subscribe<WindowSizeChangedEventHandler>(Window.Current, nameof(Window.Current.SizeChanged), MainWindow_SizeChanged);
+
+            // Set initial value
+            CurrentValue = GetCurrentValue();
+        }
+
+        #endregion
+
+        ///////////////////////////////////////////////////////////////////
+        #region Private Methods
+
+        private double GetCurrentValue()
+        {
+            return Window.Current.Bounds.Height;
+        }
+
+        #endregion
+
+        ///////////////////////////////////////////////////////////////////
+        #region Event Handler
+
+        private void MainWindow_SizeChanged(object sender, WindowSizeChangedEventArgs windowSizeChangedEventArgs)
+        {
+            CurrentValue = GetCurrentValue();
+        }
+
+        #endregion
+    }
+}
