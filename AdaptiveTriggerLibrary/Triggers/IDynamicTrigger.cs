73e41477d836883315a964710fd5e5bcf5c7b71b
Stefan Over <herdosratos@googlemail.com>
2015-08-17 22:10:10 +0200

    Added dynamic and static trigger interfaces

---
+++ AdaptiveTriggerLibrary/Triggers/IDynamicTrigger.cs
@@ -0,0 +1,11 @@
+﻿namespace AdaptiveTriggerLibrary.Triggers
+{
+    /// <summary>
+    /// Triggers where the state of <see cref="IAdaptiveTrigger.IsActive"/>
+    /// may change during the application lifetime.
+    /// </summary>
+    public interface IDynamicTrigger
+    {
+         
+    }
+}
---
+++ AdaptiveTriggerLibrary/Triggers/IStaticTrigger.cs
@@ -0,0 +1,11 @@
+﻿namespace AdaptiveTriggerLibrary.Triggers
+{
+    /// <summary>
+    /// Triggers where the state of <see cref="IAdaptiveTrigger.IsActive"/>
+    /// won't change during the application lifetime.
+    /// </summary>
+    public interface IStaticTrigger : IAdaptiveTrigger
+    {
+         
+    }
+}
