daa14ca5b95b16516254c6c708c54cd24b24164c
Stefan Over <herdosratos@googlemail.com>
2015-08-17 22:27:01 +0200

    Added IDyn-/Stat-Trigger to project and added methods for IDynTrigger

--- AdaptiveTriggerLibrary/Triggers/IDynamicTrigger.cs
+++ AdaptiveTriggerLibrary/Triggers/IDynamicTrigger.cs
@@ -4,8 +4,23 @@
     /// Triggers where the state of <see cref="IAdaptiveTrigger.IsActive"/>
     /// may change during the application lifetime.
     /// </summary>
-    public interface IDynamicTrigger
+    public interface IDynamicTrigger : IAdaptiveTrigger
     {
-         
+        /// <summary>
+        /// Forces a validation on the current trigger state.
+        /// </summary>
+        void ForceValidation();
+
+        /// <summary>
+        /// Suspends all updates of the trigger by detaching the event handlers.
+        /// </summary>
+        void SuspendUpdates();
+
+        /// <summary>
+        /// Resumes all updates of the trigger by attaching the event handlers.
+        /// </summary>
+        /// <remarks>After resuming to receive updates, you should call
+        /// <see cref="ForceValidation"/> to update the current state.</remarks>
+        void ResumeUpdates();
     }
 }
